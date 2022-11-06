9c0dee9f2eac8de744ea2571823257dc08beb172
Mackenzie Ross <115119813+mackgross@users.noreply.github.com>
2022-11-05 22:42:32 -0500

    building chatbot

---
+++ Chatty/app.py
@@ -0,0 +1,24 @@
+# import necessary libraries
+from flask import Flask, render_template, request
+
+# import Chat_T from chat.py
+from chat import Chat_T
+
+# from myFlask.chat import chat
+# import chat
+
+#class App:
+app = Flask(__name__)
+
+@app.route('/')
+def homepage():
+    return render_template("main.html");
+
+@app.route("/get")
+def get_bot_response():
+    userText = request.args.get('msg')
+    chat = Chat_T()
+    return str(chat.chat(userText))
+
+if __name__ == "__main__":
+    app.run()
